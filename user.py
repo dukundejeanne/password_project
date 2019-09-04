70fde8a5e6391b79cd200d50dd5516109216cc2c
chastine1 <batamurizashastine@gmail.com>
2019-09-04 17:08:03 +0200

    add docstring

--- user.py
+++ user.py
@@ -2,4 +2,13 @@ class User:
     """
     class that generates a new user to story there information
     """
+    user_list=[] #empty user list
+
+    def __init__(self,first_name,last_name,phone_number,email):
+        #docstring removed for simplicity
+
+        self.first_name=first_name
+        self.last_name=last_name
+        self.phone_number=phone_number
+        self.email=email
     pass
