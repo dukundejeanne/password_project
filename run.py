f09a1779a8a6c3a92723d007c31c014210412fa3
chastine1 <batamurizashastine@gmail.com>
2019-09-06 09:19:04 +0200

    save and create function in run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3.6
+#!/usr/bin/env python3.6
 from user import User
 from user import Credential
 
@@ -21,4 +21,19 @@ def checks_user(first_name,password):
     '''
     checking_user=Credential.check_user(first_name,password)
     return checking_user
+
+def create_cred(first_name,site_name,account_name,password):
+    '''
+    new credential account
+    '''
+    new_credential=Credential(first_name,site_name,account_name,password)
+    return new_credential
+def save_credential(credential):
+    '''
+    to save all credentials
+    '''
+    Credential.save_credential(credential)
+
+
+
 
