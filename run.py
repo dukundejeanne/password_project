cf7154c8b673f3f700cec0fc0f7e10cdc320fe4c
chastine1 <batamurizashastine@gmail.com>
2019-09-06 10:03:27 +0200

    add main function in run

--- run.py
+++ run.py
@@ -48,8 +48,58 @@ def copy_credential(site_name):
     return Credential.copy_credential(site_name)
 
 def main():
-    print('')
-    
+    print(' ')
+    print('welcome to the password Locker application')
+    while True:
+        print(' ')
+        print("-"*60)
+        print("use these codes to navigate: \n ca - Create account \n li - Login \n ex - Exit")
+
+        short_code = input('Please Enter your choice:').lower().strip()
+        if short_code == 'ex':
+            break
+        elif short_code == 'ca':
+            
+            print("-"*60)
+            print(' ')
+            print("Create a new account:")
+            first_name=input('Enter First_name: ').strip()
+            username=input('Enter the Username: ').strip()
+            password=input('Enter the Password: ').strip()
+            email=input('Enter your Email: ').strip()
+            save_user(create_user(first_name,username,password,email))
+            print(" ")
+            print(f'{first_name} {username} {email} {password} created successfully ')
+        elif short_code == 'li':
+            print("-"*60)
+            print(' ')
+            print("Login account:")
+          
+            username=input('Enter your Username: ').strip()
+            password=str(input('Enter the Password: '))
+            user_exists== checks_user(first_name,username,password,email)
+            if user_exists == username:
+                print(" ")
+                print(f'Welcome {username}.Please choose to continue.')
+                print('')
+                while True:
+                    print("-"*60)
+                    print('Navigation code: \n cc-Create a Credential \n dc-Display Credentials \n cp-Copy Password \n ex-Exit')
+                    short_code=input('Please Enter your choice: ').lower().strip()
+                    print("-"*60)
+                    if short_code == 'ex':
+                        print(" ")
+                        print(f'Bye {first_name}')
+                        break
+                    # elif short_code =='cc':
+                    #     print
+          
+            save_user(create_user(first_name,username,password,email))
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
